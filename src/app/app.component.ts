1f62fbbc694076752d3b63433c09fa62b6e5a802
Omolara5861 <debbiegterra@gmail.com>
2023-03-20 14:47:03 +0100

    feat: :sparkles: Fetch data from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Photo } from './model/photo';
+import { PhotoService } from './service/photo.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'products-infinite-scrool';
+  title = 'products-infinite-scroll';
+
+  photosArray!: Photo;
+
+  constructor(private photoService: PhotoService) {
+    this.photoService.getAllPhotos().subscribe((photos: any) => {
+      this.photosArray = photos;
+      console.log(this.photosArray);
+    },
+    (err) => {
+      console.log(err);
+    })
+  }
+
 }
