48acbced31c88c62fb8bda4429dcbb33765288ba
Omolara5861 <debbiegterra@gmail.com>
2023-03-20 16:16:21 +0100

    fix:

--- src/app/service/cat.service.ts
+++ src/app/service/cat.service.ts
@@ -1,17 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Photo } from '../model/photo';
+import { Cat } from '../model/cat';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PhotoService {
+export class CatService {
 
   constructor(private http: HttpClient) { }
 
-  getAllPhotos(): Observable<Photo[]>{
-    return this.http.get<Photo[]>('https://jsonplaceholder.typicode.com/photos');
+  getAllCats(page: number): Observable<Cat[]>{
+    return this.http.get<Cat[]>(`https://api.thecatapi.com/v1/breeds?page=${page}&limit=10`) as Obser;
   }
 
 }
