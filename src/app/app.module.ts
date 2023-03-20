138f349aa1554f73294716cd67299962c7c1c5bd
Omolara5861 <debbiegterra@gmail.com>
2023-03-20 15:57:23 +0100

    chore: :tada: Create component for ngx method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {  HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { NgxInfiniteScrollComponent } from './ngx-infinite-scroll/ngx-infinite-scroll.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgxInfiniteScrollComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ngx-infinite-scroll/ngx-infinite-scroll.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngx-infinite-scroll',
+  templateUrl: './ngx-infinite-scroll.component.html',
+  styleUrls: ['./ngx-infinite-scroll.component.css']
+})
+export class NgxInfiniteScrollComponent {
+
+}
