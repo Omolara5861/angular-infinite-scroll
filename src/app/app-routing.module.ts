89527b5af76423f93d53b3554d4b38938307a894
Omolara5861 <debbiegterra@gmail.com>
2023-03-21 05:29:27 +0100

    chore: :truck: Move component to app folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NgxInfiniteScrollComponent } from '../components/ngx-infinite-scroll/ngx-infinite-scroll.component';
-import { IntersectionObserverAPIComponent } from '../components/intersection-observer-api/intersection-observer-api.component';
+import { NgxInfiniteScrollComponent } from './components/ngx-infinite-scroll/ngx-infinite-scroll.component';
+import { IntersectionObserverAPIComponent } from './components/intersection-observer-api/intersection-observer-api.component';
 
 const routes: Routes = [
   {path: 'ngx-scroll', component: NgxInfiniteScrollComponent},
